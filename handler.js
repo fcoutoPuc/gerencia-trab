f97dbefd9055792a25f08e8eb5410c9b76c33041
fcoutoPuc <fernandocouto@MacBook-Pro.local>
2024-12-01 23:55:51 -0300

    remove comments

--- handler.js
+++ handler.js
@@ -12,23 +12,15 @@ app.use(express.json());
 
 app.post('/products', createProduct);
 
-// Route to get a product by ID
+
 app.get('/products/:id', getProduct);
 
-// Route to update a product by ID
 app.put('/products/:id', updateProduct);
 
-// Route to delete a product by ID
 app.delete('/products/:id', deleteProduct);
 
 
-// Route to list all products
 app.get('/products', listProducts);
-
-// app.get('/products', async (req, res) => {
-//     // const { name, amount, price } = req.body;
-//     return res.status(200).json({ productCreated: `vaii` })
-// })
 
 
 app.use((req, res, next) => {
