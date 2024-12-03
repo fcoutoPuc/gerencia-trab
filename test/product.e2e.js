671f0b311a04dc0e4c9af7086c75c6bddc360a1b
fcoutoPuc <fernandocouto@MacBook-Pro.local>
2024-12-03 17:26:59 -0300

    should fail the integration test

--- test/product.e2e.js
+++ test/product.e2e.js
@@ -1,17 +1,13 @@
 import request from 'supertest';
 
 describe('GET /teste/products', () => {
-    // it('should return a JSON response with name "Hello"', async () => {
-    //     const response = await request('http://localhost:3000')
-    //         .get('/teste/products')
-    //         .expect(200);
-
-    //     expect(response.body).toEqual({
-    //         name: 'Hello'
-    //     });
-    // });
- 
     it('should return a JSON response with name "Hello"', async () => {
-        expect(true).toEqual(true);
+        const response = await request('http://localhost:3000')
+            .get('/teste/products')
+            .expect(200);
+
+        expect(response.body).toEqual({
+            name: 'Hello'
+        });
     });
 });
